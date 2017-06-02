4d7cee48d4066eb81bd5a1b861c098f9f47ffaa4
murabye <vlada_kuz2011@mail.ru>
2017-06-02 05:38:04 +0500

    определение порядка матрицы

--- ex5/Program.cs
+++ ex5/Program.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyLib;
 
 namespace ex5
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
+            while (true)
+            {
+                Console.WriteLine("Введите матрицу построчно через пробелы: ");
+                var first = Console.ReadLine();
+                if (first == "")
+                {
+                    Console.WriteLine("Неверный ввод: ");
+                    continue;
+                }
+
+                var count = first.Split(' ').Length;
+
+            }
         }
     }
 }
